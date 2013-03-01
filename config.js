9186f1449f3d2505b12fca2de3267760213fe02e
Mark Ibbotson <uittilla@gmail.com>
2013-03-01 16:59:47 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,12 +2,12 @@
 //ec2-176-34-76-115.eu-west-1.compute.amazonaws.com
 module.exports = {
     bs: {
-        "host": "ec2-54-247-53-163.eu-west-1.compute.amazonaws.com",
+        "host": "<you wish>",
         "port": 11300
     },
 
     mongo : {
-        "host": "ec2-54-246-10-250.eu-west-1.compute.amazonaws.com"
+        "host": "<you wish>"
     },
 
     DEBUG : false,
